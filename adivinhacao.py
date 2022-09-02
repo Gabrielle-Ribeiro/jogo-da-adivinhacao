c8046d97e7fa7b759301640647a9864953ddbcc8
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2022-09-02 13:18:26 -0300

    Adiciona laço de repetição para tentativas

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,21 +4,24 @@ print("********************************")
 print("")
 
 numero_secreto = 42
+total_de_tentativas = 3
 
-chute = input("Digite o seu número: ")
-print("Você digitou: ", chute)
+for rodada in range(1,4):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute = input("Digite o seu número: ")
+    print("Você digitou: ", chute)
 
-chute = int(chute)
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    chute = int(chute)
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if(acertou):
-    print("Você acertou!")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior que o número secreto.")
-    elif(menor):
-        print("Você errou! O seu chute foi menor que o número secreto.")
+    if(acertou):
+        print("Você acertou!")
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
 
 print("\nFim do jogo")
