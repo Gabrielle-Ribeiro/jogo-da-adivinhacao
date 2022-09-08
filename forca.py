4f1f9ebd74691837cca98727b66017167d47bff5
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2022-09-08 18:03:39 -0300

    Adiciona estrutura inicial do jogo da forca

--- forca.py
+++ forca.py
@@ -4,6 +4,25 @@ def jogar():
     print("**************************")
     print("")
 
+    palavra_secreta = "python"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print(letras_acertadas)
+
+        chute = input("Qual a letra? ")
+
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+            index = index + 1
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
