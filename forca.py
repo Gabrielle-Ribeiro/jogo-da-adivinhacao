f400d44745e402bc95520631d66dc352afb39489
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2022-09-13 16:59:10 -0300

    Adiciona encerramento de jogo

--- forca.py
+++ forca.py
@@ -4,24 +4,35 @@ def jogar():
     print("**************************")
     print("")
 
-    palavra_secreta = "python"
+    palavra_secreta = "python".upper()
     letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
+    erros = 0
 
-    while(not enforcou and not acertou):
+    while not enforcou and not acertou:
         print(letras_acertadas)
 
         chute = input("Qual a letra? ")
+        chute = chute.strip().upper()
 
-        chute = chute.strip()
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute.upper() == letra.upper():
+                    letras_acertadas[index] = letra
+                index = index + 1
+        else:
+            erros += 1
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+
+    if acertou:
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
 
     print("Fim do jogo")
 
